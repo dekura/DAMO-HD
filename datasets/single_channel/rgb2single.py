c87fd2fc1eb4a73a64f12e76c1008d727ad91908
Guojin Chen <cgjhaha@qq.com>
2021-08-28 10:35:28 +0800

    ✨ feat: update single channel testing

---
+++ datasets/single_channel/rgb2single.py
@@ -0,0 +1,56 @@
+'''
+Author: Guojin Chen @ CUHK-CSE
+Homepage: https://dekura.github.io/
+Date: 2021-08-25 08:28:29
+LastEditTime: 2021-08-25 12:09:08
+Contact: cgjhaha@qq.com
+Description: translate the rgb image to single channel images.
+'''
+
+import torch
+import torchvision
+from tqdm import tqdm
+import numpy as np
+from PIL import Image
+from pathlib import Path
+
+
+in_dir = '/research/d4/gds/gjchen21/datasets/datasets/dlsopc_datasets/via_layouts_0.4_unpaired/dls'
+out_dir = '/research/d4/gds/gjchen21/datasets/datasets/dlsopc_datasets/via_layouts_0.4_unpaired/dls_single'
+
+in_dir = Path(in_dir)
+out_dir = Path(out_dir)
+
+
+def rgb2binary(in_dir: Path, out_dir: Path):
+    for x in in_dir.iterdir():
+        x_name = x.name
+        out_x = out_dir / x_name
+        Path.mkdir(out_x, exist_ok=True)
+        for img in tqdm(list(x.glob('*.png'))):
+            img_name = img.name
+            out_img = out_x / img_name
+            p = Image.open(str(img)).convert('RGB')
+            r = p.getchannel('R')
+            g = p.getchannel('G')
+            b = p.getchannel('B')
+            r = np.asarray(r)
+            r[np.where(r>1)] = 1
+            g = np.asarray(g)
+            g[np.where(g>1)] = 1
+            b = np.asarray(b)
+            b[np.where(b>1)] = 1
+            x, y = r.shape
+            out_p = np.zeros(r.shape)
+            out_p[np.where(r == 1)] = 1
+            out_p[np.where(g == 1)] = 1
+            out_p[np.where(b == 1)] = 1
+            out_p = out_p*255
+            out_p = Image.fromarray(out_p).convert('1')
+            out_p.save(str(out_img))
+
+
+
+
+if __name__ == '__main__':
+    rgb2binary(in_dir, out_dir)
---
+++ train_dlspixhd_haoyu_e100_m2c.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#SBATCH --job-name=dmo4
+#SBATCH --mail-user=cgjhaha@qq.com
+#SBATCH --mail-type=ALL
+#SBATCH --output=/research/dept7/glchen/tmp/log/ovia2pixhd_e100_dr2mg.txt
+#SBATCH --gres=gpu:4
+# this script is design to train a 2048*2048
+# design come from the ganopc dataset
+# the mask is generated from the deep levelset generator
+
+
+/research/d4/gds/gjchen21/miniconda3/envs/pytorch/bin/python train.py \
+--gpu_ids 0,1,2,3 \
+--checkpoints_dir /research/d4/gds/gjchen21/github/DAMO-HD/checkpoints \
+--dataroot /research/d4/gds/gjchen21/datasets/datasets/dlsopc_datasets/via_layouts_0.4_unpaired/dls_single \
+--netG global \
+--batchSize 4 \
+--resize_or_crop resize_and_crop \
+--loadSize 1024 \
+--fineSize 1024 \
+--niter 50 \
+--niter_decay 50 \
+--print_freq 100 \
+--input_nc 3 \
+--output_nc 3 \
+--norm batch \
+--name dls_haoyu_e100_m2c \
+--label_nc 0 \
+--no_instance
